ae2dd1c0fad0d3d524fa54e6b01fec7bb8efd69f
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-02-08 21:22:40 +0200

    content interaction,  text content, inner html, set and grab attributes

--- content_interaction.js
+++ content_interaction.js
@@ -1,1 +1,7 @@
+var p = document.querySelector('p')
+p.textContent = "new text"
+p.innerHTML = "<strong> I am bold <\strong>"
 
+var special = document.querySelector('#special')
+var specialA = special.querySelector("a")
+specialA.setAttribute('href', 'www.amazon.com')
