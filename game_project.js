f2e57d9d3d946300ff158eaf25ce61259dbb14c0
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-02-09 11:58:25 +0200

    Update game_project.js
    chnaged the square markers

--- game_project.js
+++ game_project.js
@@ -14,3 +14,31 @@ function clear(){
 }
 
 restart.addEventListener('click', clear);
+
+//check the square marker
+
+// var cellOne = document.querySelector('#one');
+//
+// cellOne.addEventListener('click',function(){
+//   if (cellOne.textContent === '') {
+//     cellOne.textContent = "X";
+//   }else if (cellOne.textContent === "X") {
+//     cellOne.textContent = "O";
+//   }else {
+//     cellOne.textContent = '';
+//   }
+// })
+
+function changeMarker(){
+  if (this.textContent === '') {
+    this.textContent = 'X';
+  }else if (this.textContent === 'X') {
+    this.textContent = "O"
+  } else {
+    this.textContent = '';
+  }
+}
+
+for (var i = 0; i < squares.length; i++) {
+  squares[i].addEventListener('click', changeMarker)
+}
